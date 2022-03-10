08f144d9eb593e6f05a50557582861c583517a0f
Aylin Gyoren <aylingyoren21@gmail.com>
2022-03-10 17:27:39 +0300

    init commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <form>
+        <input type="text" />
+        <input type="number" />
+      </form>
+      <h1>GraphQL App</h1>
+    </div>
+  );
+}
+
+export default App;
